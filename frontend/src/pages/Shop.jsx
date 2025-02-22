7a187e6360c2b94b3a5b78f659fb80f4bc131ab8
mehedi <mollameehedi@gmail.com>
2025-02-23 00:26:23 +0600

    about page design complete

--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Container from '../components/utils/Container'
 import ShopSidbar from '../components/screens/shop/ShopSidbar'
 import AllProduct from '../components/screens/shop/AllProduct'
@@ -6,6 +6,18 @@ import { CiFilter } from "react-icons/ci";
 
 const Shop = () => {
   const [filter, setFilter] = useState(false);
+  useEffect(() => {
+    function ResizeScreen(){
+       
+      if(window.innerWidth < 667){
+        setFilter(false)
+      }else{
+        setFilter(true)
+      }
+    }
+    
+    window.addEventListener("resize",ResizeScreen)
+  },[])
   return (
     <section className='mt-5'>
         <Container>
@@ -13,14 +25,7 @@ const Shop = () => {
           <div className="grid grid-cols-12">
             <div className="lg:col-span-3 col-span-12 ">
           <h3 className='flex justify-between text-2xl p-2 bg-white lg:hidden'><span>Filter</span> <CiFilter onClick={() => setFilter(!filter)} className='lg:hidden text-white bg-primary p-1 text-4xl'/></h3>
-          <div className='lg:hidden'>
-            {filter ?  <ShopSidbar/>: ''}
-
-          </div>
-          <div className='hidden lg:block'>
-            <ShopSidbar/>
-
-          </div>
+          {filter ?  <ShopSidbar/>: ''}
             </div>
             <div className='lg:col-span-9 col-span-12'> 
                 <AllProduct/>
