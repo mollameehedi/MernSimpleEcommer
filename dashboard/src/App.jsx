cd6833b84fe543b2c0e4e2ad1f091cfa3b4b6f93
mehedi <mollameehedi@gmail.com>
2025-03-01 00:35:35 +0600

    react router setup

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route, RouterProvider } from "react-router";
+import Home from './pages/Home';
+import About from './pages/About';
 
 const App = () => {
+
+ 
   return (
-    <>
-    
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
