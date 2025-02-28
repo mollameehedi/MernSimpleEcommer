82a9e114d96ca0f2ee3c2e38742287fc09914923
mehedi <mollameehedi@gmail.com>
2025-02-28 23:46:49 +0600

    some change

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className="bg-red-500">
-      <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
-        Submit
-      </button>
-    </div>
+    <>
+    
+    </>
   );
 }
 
