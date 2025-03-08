f25e5dfef95c113c8d16111ac590b8416633277b
mehedi <mollameehedi@gmail.com>
2025-03-08 23:59:28 +0600

    login and signup page design complete

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router";
 import Home from './pages/Home';
 import About from './pages/About';
+import Login from './pages/auth/Login';
+import Signup from './pages/auth/Signup';
 
 const App = () => {
   return (
@@ -9,6 +11,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup  />} />
       </Routes>
     </BrowserRouter>
   );
--- dashboard/src/pages/Home.jsx
+++ dashboard/src/pages/Home.jsx
@@ -1,11 +1,12 @@
+import { Button } from '@material-tailwind/react';
 import React from 'react'
 
 const Home = () => {
   return (
     <div>
-     HOme
-     </div>
-  )
+      <Button>Button</Button>;
+    </div>
+  );
 }
 
 export default Home
